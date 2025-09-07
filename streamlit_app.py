23c9dbdd217203a2da9f7e7d6146f5e10dc03302
Thivya Tamil Selvam <80906383+ThivyaTS@users.noreply.github.com>
2025-09-07 17:57:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ warnings.filterwarnings("ignore")  # optional: ignore all other warnings
 st.set_page_config(
     page_title="Stock Dashboard",
     layout="wide",  # makes the page full-width
-    set_blurred_background("bg_thivya_web.jpg", blur_px=6)
+    set_blurred_background("bg_thivya_web.jpg", blur_px=6),
 )
 
 
