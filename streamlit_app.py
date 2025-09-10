1858c094c3961c8ea571fa42ae3d0cbaccb4ee78
Thivya Tamil Selvam <80906383+ThivyaTS@users.noreply.github.com>
2025-09-10 14:06:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,14 +41,14 @@ import base64
 
 from st_files_connection import FilesConnection
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
+# # Create connection object and retrieve file contents.
+# # Specify input format is a csv and to cache the result for 600 seconds.
+# conn = st.connection('gcs', type=FilesConnection)
+# df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.Owner} has a :{row.Pet}:")
 
 def set_fixed_background(image_path):
     with open(image_path, "rb") as f:
