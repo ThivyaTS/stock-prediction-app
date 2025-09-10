3b7db557d8c626840cbdaff45280ab658a1665c3
Thivya Tamil Selvam <80906383+ThivyaTS@users.noreply.github.com>
2025-09-10 23:06:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ with col1:
         ),
         template='plotly_white',
         height=500,
-        plot_bgcolor='rgba(2, 54, 61,0.7)',   # chart area: black, 70% opacity
+        plot_bgcolor='rgba(29, 37, 38,0.7)',   # chart area: black, 70% opacity
         paper_bgcolor='rgba(0,0,0,0.0)'   # outside area: black, 70% opacity
 
     )
