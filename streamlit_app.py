8bbd37bb597e7eb7373d00292bf196c079c8ae22
Thivya Tamil Selvam <80906383+ThivyaTS@users.noreply.github.com>
2025-09-13 14:28:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -491,14 +491,14 @@ if st.button("🔮 Predict Next Day Close Price"):
                 or chunk.candidates[0].content.parts is None
             ):
                 continue
-            # if chunk.candidates[0].content.parts[0].inline_data and chunk.candidates[0].content.parts[0].inline_data.data:
-            #     file_name = f"ENTER_FILE_NAME_{file_index}"
-            #     file_index += 1
-            #     inline_data = chunk.candidates[0].content.parts[0].inline_data
-            #     data_buffer = inline_data.data
-            #     file_extension = mimetypes.guess_extension(inline_data.mime_type)
-            #     save_binary_file(f"{file_name}{file_extension}", data_buffer)
-            # else:
+            if chunk.candidates[0].content.parts[0].inline_data and chunk.candidates[0].content.parts[0].inline_data.data:
+                file_name = f"ENTER_FILE_NAME_{file_index}"
+                file_index += 1
+                inline_data = chunk.candidates[0].content.parts[0].inline_data
+                data_buffer = inline_data.data
+                file_extension = mimetypes.guess_extension(inline_data.mime_type)
+                save_binary_file(f"{file_name}{file_extension}", data_buffer)
+            else:
                  generated_text += chunk.text + " "  # <--- Accumulate text chunks
         return generated_text.strip()  # Return full concatenated text
       
